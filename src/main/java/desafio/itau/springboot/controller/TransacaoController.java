e7fcb0f9d279bc5800bbfa58af8acdbda1934469
HiagoCosta1 <hiagocostazero@gmail.com>
2026-02-25 17:04:57 -0300

    Criacao do controller e adicao de metodos delete e post

---
+++ src/main/java/desafio/itau/springboot/controller/TransacaoController.java
@@ -0,0 +1,32 @@
+package desafio.itau.springboot.controller;
+
+import desafio.itau.springboot.dto.TransacaoRequest;
+import desafio.itau.springboot.model.Transacao;
+import desafio.itau.springboot.service.TransacaoService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/transacao")
+public class TransacaoController {
+
+    private final TransacaoService transacaoService;
+
+    public TransacaoController(TransacaoService transacaoService){
+        this.transacaoService = transacaoService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Void> salvar(@RequestBody TransacaoRequest dto){
+        Transacao novo = new Transacao(dto.getValor(), dto.getDataHora());
+        transacaoService.salvar(novo);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    @DeleteMapping
+    public ResponseEntity<Void> deletar(){
+        transacaoService.deletar();
+        return ResponseEntity.ok().build();
+    }
+}
